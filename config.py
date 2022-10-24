d794f3aebdc287a9d63410556d587e839a5a917c
youssefwael <youssefwael397>
2022-10-24 18:15:23 +0200

    add all requirements and heroku mysql uri

--- config.py
+++ config.py
@@ -2,9 +2,21 @@
 import pymysql
 import os
 
-host='localhost'
-user='root'
-password= ''
-db= 'shebak'
+# host='localhost'
+# user='root'
+# password= ''
+# db= 'shebak'
+
+# mysql_uri = f'mysql+pymysql://{user}:{password}@{host}:3306/{db}'
+
+
+# heroku shebak-db => clear db mysql_uri
+# 'mysql://b6882d20dfbdb0:553e7660@us-cdbr-east-06.cleardb.net/heroku_8f810f4920e9790?reconnect=true'
+
+host = 'us-cdbr-east-06.cleardb.net'
+user = 'b6882d20dfbdb0'
+password = '553e7660'
+db = 'heroku_8f810f4920e9790'
+
 
 mysql_uri = f'mysql+pymysql://{user}:{password}@{host}:3306/{db}'
